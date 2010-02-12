ad65ff934ccf2e9f1e0b5ed90feee7bcfce61045
Ammar Qammaz <ammarkov@gmail.com>
2010-02-12 04:22:51 +0200

    small change

--- TextAnalyzer.h
+++ TextAnalyzer.h
@@ -7,10 +7,20 @@
 
 #include "WordCollection.h"
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 word_collection * TextAnalyzer_NewWordCollection();
 unsigned int TextAnalyzer_WordOccurances(word_collection * acol,unsigned char * theword , unsigned int wordsize);
 void TextAnalyzer_DeleteWordCollection(word_collection * acol);
 
 unsigned int ClearTextFromHTMLTags(word_collection * acol,char * text,unsigned int &textsize);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // TEXTANALYZER_H_INCLUDED
